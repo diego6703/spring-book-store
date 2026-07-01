430625b84d5c7e8f61964a6148a74b9ddce40f0e
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-01 16:50:45 +0200

    add CommandLineRunner for manual testing

--- src/main/java/p/projects/springbookstore/SpringBookStoreApplication.java
+++ src/main/java/p/projects/springbookstore/SpringBookStoreApplication.java
@@ -1,7 +1,13 @@
 package p.projects.springbookstore;
 
+import java.math.BigDecimal;
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import p.projects.springbookstore.model.Book;
+import p.projects.springbookstore.service.BookService;
 
 @SpringBootApplication
 public class SpringBookStoreApplication {
@@ -10,4 +16,20 @@ public class SpringBookStoreApplication {
         SpringApplication.run(SpringBookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(BookService bookService) {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("The Great Gatsby");
+            book.setAuthor("F. Scott Fitzgerald");
+            book.setIsbn("1234567890");
+            book.setPrice(new BigDecimal("19.99"));
+
+            bookService.save(book);
+            System.out.println("Saved book: " + book.getTitle());
+
+            List<Book> allBooks = bookService.findAll();
+            System.out.println("Books in database: " + allBooks.size());
+        };
+    }
 }

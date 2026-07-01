239b03900aa70bf1a82a55484b170307b1b908ff
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-01 17:44:42 +0200

    fix: move test configuration to src/test/resources

--- src/test/java/p/projects/springbookstore/SpringBookStoreApplicationTests.java
+++ src/test/java/p/projects/springbookstore/SpringBookStoreApplicationTests.java
@@ -2,8 +2,10 @@ package p.projects.springbookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBookStoreApplicationTests {
 
     @Test

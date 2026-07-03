bb33e3a95a03d25b55ca7b9032a54270102a9cdb
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-03 16:37:47 +0200

    refactor(repository): replace manual implementation with JpaRepository
    deleted BoorRepositoryImpl

--- src/main/java/p/projects/springbookstore/repository/BookRepository.java
+++ src/main/java/p/projects/springbookstore/repository/BookRepository.java
@@ -1,14 +1,7 @@
 package p.projects.springbookstore.repository;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import p.projects.springbookstore.model.Book;
 
-public interface BookRepository {
-
-    Book save(Book book);
-
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
+public interface BookRepository extends JpaRepository<Book,Long> {
 }

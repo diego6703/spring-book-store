cc331f006828cdbf95ab481efbda5a42caed83dd
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-13 15:44:24 +0200

    feat: add Authentication service and repository method

--- src/main/java/p/projects/springbookstore/repository/UserRepository.java
+++ src/main/java/p/projects/springbookstore/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package p.projects.springbookstore.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import p.projects.springbookstore.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

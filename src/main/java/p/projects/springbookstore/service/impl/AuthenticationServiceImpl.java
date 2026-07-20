96f49b2591edab362c4bb2266133485c0439b1b2
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-20 16:01:57 +0200

    feat: add default role to new users and initial database data

--- src/main/java/p/projects/springbookstore/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/p/projects/springbookstore/service/impl/AuthenticationServiceImpl.java
@@ -1,20 +1,28 @@
 package p.projects.springbookstore.service.impl;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import p.projects.springbookstore.dto.UserRegistrationRequestDto;
 import p.projects.springbookstore.dto.UserResponseDto;
+import p.projects.springbookstore.exception.EntityNotFoundException;
 import p.projects.springbookstore.exception.RegistrationException;
 import p.projects.springbookstore.mapper.UserMapper;
+import p.projects.springbookstore.model.Role;
+import p.projects.springbookstore.model.RoleName;
 import p.projects.springbookstore.model.User;
+import p.projects.springbookstore.repository.RoleRepository;
 import p.projects.springbookstore.repository.UserRepository;
 import p.projects.springbookstore.service.AuthenticationService;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto request) {
@@ -23,6 +31,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         }
 
         User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+
+        Role userRole = roleRepository.findByName(RoleName.USER)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find role USER"));
+        user.setRoles(Set.of(userRole));
+
         return userMapper.toDto(userRepository.save(user));
     }
 }

37864e383b02f654c6255858c5a05bbb36d622a6
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-23 14:50:34 +0200

    feat: add DTOs for user login
    Dodanie UserLoginRequestDto oraz UserLoginResponseDto.

---
+++ src/main/java/p/projects/springbookstore/dto/UserLoginRequestDto.java
@@ -0,0 +1,12 @@
+package p.projects.springbookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String password
+) {}

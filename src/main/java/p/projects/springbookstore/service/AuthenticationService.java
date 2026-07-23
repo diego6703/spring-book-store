74d822b31bf02ecf65d78ade77a46dbcee1de189
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-07-23 17:11:27 +0200

    feat: add LoginException and update AuthenticationService interface

--- src/main/java/p/projects/springbookstore/service/AuthenticationService.java
+++ src/main/java/p/projects/springbookstore/service/AuthenticationService.java
@@ -1,9 +1,13 @@
 package p.projects.springbookstore.service;
 
+import p.projects.springbookstore.dto.UserLoginRequestDto;
+import p.projects.springbookstore.dto.UserLoginResponseDto;
 import p.projects.springbookstore.dto.UserRegistrationRequestDto;
 import p.projects.springbookstore.dto.UserResponseDto;
 
 public interface AuthenticationService {
 
     public UserResponseDto register(UserRegistrationRequestDto request);
+
+    public UserLoginResponseDto login(UserLoginRequestDto request);
 }
